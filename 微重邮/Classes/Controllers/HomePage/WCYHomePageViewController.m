8d8ba0bb63d5c38950ba6056558760b919541dba
WBoxer <1051851627@qq.com>
2016-10-18 21:37:38 +0800

    添加滑动背景

--- 微重邮/Classes/Controllers/HomePage/WCYHomePageViewController.m
+++ 微重邮/Classes/Controllers/HomePage/WCYHomePageViewController.m
@@ -8,18 +8,47 @@
 
 #import "WCYHomePageViewController.h"
 
-@interface WCYHomePageViewController ()
+@interface WCYHomePageViewController () <UIScrollViewDelegate, UICollectionViewDelegate, UICollectionViewDataSource>
 
+@property (strong, nonatomic) UIScrollView *backgroundScrollView;  /**< 背景下拉板 */
+@property (strong, nonatomic) UICollectionView *functionCollectionView;  /**< 功能板块 */
 @end
 
 @implementation WCYHomePageViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initUI];
 }
 
 - (void)initUI
 {
     self.view.backgroundColor = kAppBg_Color;
+    [self initBackgroundScrollView];
+}
+
+- (void)initBackgroundScrollView
+{
+    self.backgroundScrollView = [[UIScrollView alloc] init];
+    self.backgroundScrollView.backgroundColor = kCommonWhite_Color;
+    self.backgroundScrollView.delegate = self;
+    self.backgroundScrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height);
+    self.backgroundScrollView.contentOffset = CGPointMake(0, 0);
+    [self.view addSubview:self.backgroundScrollView];
+    [self.backgroundScrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.and.right.and.top.bottom.equalTo(self.view);
+    }];
+    
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 4;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [self.functionCollectionView dequeueReusableCellWithReuseIdentifier:@"functionCollectionViewCellID" forIndexPath:indexPath];
+    return cell;
 }
 @end
